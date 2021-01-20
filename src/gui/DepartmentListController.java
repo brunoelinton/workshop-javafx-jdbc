5a3ef2fc2eb05848223880e4e0e7dc6ab87d926b
brunoelinton <brunoelinton@outlook.com>
2021-01-20 01:13:31 -0300

    Initializing action as parameter

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -19,23 +19,25 @@ import model.entities.Department;
 import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
-	private DepartmentService service = new DepartmentService();
+	
+	private DepartmentService service;						// VARIÁVEL PARA RECEBER A INSTANCIA DO SERVIÇO RESPONSÁVEL POR RETORNAR A LISTA DE DEPARTAMENTOS
 	
 	/* ELEMENTOS DA TELA DE DEPARTAMENTO */
 	@FXML
-	private TableView<Department> tableViewDepartment;			// TABELA QUE LISTA OS DEPARTAMENTOS
+	private TableView<Department> tableViewDepartment;		// TABELA QUE LISTA OS DEPARTAMENTOS
 	
 	@FXML
-	private TableColumn<Department, Integer> tableColumnId;		//	COLUNA REFERENTE AO ID DO DEPARTAMENTO
+	private TableColumn<Department, Integer> tableColumnId;	//	COLUNA REFERENTE AO ID DO DEPARTAMENTO
 	
 	@FXML
-	private TableColumn<Department, String> tableColumnName;	//	COLUNA REFERENTE AO NOME DO DEPARTAMENTO
+	private TableColumn<Department, String> tableColumnName;//	COLUNA REFERENTE AO NOME DO DEPARTAMENTO
 	
 	@FXML
-	private Button btNew;										// BOTÃO PARA INSERIR UM NOVO DEPARTAMENTO
+	private Button btNew;									// BOTÃO PARA INSERIR UM NOVO DEPARTAMENTO
 	
-	private ObservableList<Department> obsList;
+	private ObservableList<Department> obsList;				// VARIÁVEL DO JAVAX QUE IRÁ ARMAZENAR A LISTA DE DEPARTAMENTOS
 	
+	// MÉTODO QUE FAZ UMA INJEÇÃO DE DEPENDÊNCIA INSTANCIANDO O SERVIÇO QUE IRÁ NOS RETORNAR A LISTA DE DEPARTAMENTOS
 	public void setDepartmentService(DepartmentService department) {
 		this.service = department;
 	}
@@ -62,13 +64,14 @@ public class DepartmentListController implements Initializable {
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 	}
 	
+	// MÉTODO QUE ACESSA O SERVIÇO DE DEPARTAMENTO, CARREGAR OS DEPARTAMENTOS E INSERÍ-LOS NA VARIÁVEL DO TIPO 'ObservableList' QUE É UM TIPO COMPATÍVEL COM O JAVAFX 
 	public void updateTableView() {
-		if(service == null) {
+		if(service == null) {	// VERIFICANDO SE O SERVIÇO NÃO FOI INJETADO / INICIADO
 			throw new IllegalStateException("Service was null");
 		}
-		List<Department> list = service.findAll();
-		obsList = FXCollections.observableArrayList(list);
-		tableViewDepartment.setItems(obsList);
+		List<Department> list = service.findAll();			// LISTA TEMPORÁRIA PARA ARMAZENAR A LISTA DE DEPARTAMENTOS QUE O SERVIÇO NOS TROUXE
+		obsList = FXCollections.observableArrayList(list);	// CARREGANDO A LISTA DE DEPARTAMENTOS NA LISTA COMPATÍVEL COM O JAVAFX
+		tableViewDepartment.setItems(obsList);				// ASSOCIANDO A LISTA DE DEPARTAMENTOS À 'TableView' PARA QUE ELA POSSA SER MOSTRADA NA APLICAÇÃO
 	}
 
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -15,7 +16,6 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
-import model.entities.Department;
 import model.services.DepartmentService;
 
 public class MainViewController implements Initializable {
@@ -38,12 +38,15 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {				// EVENTO PARA SELEÇÃO DO SUBITEM DE CADASTRO DE DEPARTAMENTOS
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 	}
 	
 	@FXML
 	public void onMenuItemAboutAction() {					// EVENTO PARA SELEÇÃO DO SUBITEM 'SOBRE' 
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 	
 	/*----------------------------------------------------------------------------------------------*/
@@ -56,33 +59,7 @@ public class MainViewController implements Initializable {
 	}
 	
 	// EVENTO PARA CHAMADA DE UMA OUTRA JANELA QUALQUER DA APLICAÇÃO
-	private synchronized void loadView(String absoluteName) {
-		try {
-			// INSTANCIANDO UM OBJETO DO TIPO 'FXMLLoader' PARA CARREGAMENTO DA TELA
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			// INSTANCIANDO UM CONTAINER DO TIPO 'VBox' QUE CONTÉM A JANELA A SER EXIBIDA
-			VBox newVBox = loader.load();
-			// OBTENDO A REFERÊNCIA DA CENA DA JANELA PRINCIPAL
-			Scene mainScene = Main.getMainScene();
-			// OBETENDO A REFRÊNCIA DO CONTAINER 'VBox' DA CENA DA JANELA PRINCIPAL
-			VBox mainVBox = (VBox)((ScrollPane)mainScene.getRoot()).getContent();
-			
-			/* PRESERVANDO O MENU DA JANELA PRINCIPAL */
-			// 1ª OBTENDO O PRIMEIRO ELEMENTO FILHO DO 'VBox' DA CENA DA JANELA PRINCIPAL, OU SEJA, O 'content'			
-			Node mainMenu = mainVBox.getChildren().get(0);
-			// 2ª REMOVENDO TODOS OS FILHOS DO 'VBox' DA CENA DA JANELA PRINCILA
-			mainVBox.getChildren().clear();
-			// 3ª ADICIONANDO DE VOLTA O MENU DA CENA DA JANELA PRINCIPAL
-			mainVBox.getChildren().add(mainMenu);
-			// 4ª ADICIONANDO TODOS OS FILHOS DO 'VBox' DA JANELA A SER EXIBA, DENTRO DA SCENA DA JANELA PRINCIPAL
-			mainVBox.getChildren().addAll(newVBox.getChildren());
-		} catch(IOException e) {	// TRATANDO ERRO CASO HAJA ALGUM PROBLEMA NO CARRRGAMENTO DA TELA
-			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
-		}
-	}
-	
-	// EVENTO PARA CHAMADA DE UMA OUTRA JANELA QUALQUER DA APLICAÇÃO
-	private synchronized void loadView2(String absoluteName) {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initialingAction) {
 		try {
 			// INSTANCIANDO UM OBJETO DO TIPO 'FXMLLoader' PARA CARREGAMENTO DA TELA
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
@@ -103,9 +80,8 @@ public class MainViewController implements Initializable {
 			// 4ª ADICIONANDO TODOS OS FILHOS DO 'VBox' DA JANELA A SER EXIBA, DENTRO DA SCENA DA JANELA PRINCIPAL
 			mainVBox.getChildren().addAll(newVBox.getChildren());
 			
-			DepartmentListController controller = loader.getController();
-			controller.setDepartmentService(new DepartmentService());
-			controller.updateTableView();
+			T controller = loader.getController();
+			initialingAction.accept(controller);
 		} catch(IOException e) {	// TRATANDO ERRO CASO HAJA ALGUM PROBLEMA NO CARRRGAMENTO DA TELA
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
